7ee823fa66b9b02788d5abf82a31e8e03d6c9d2b
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-11-27 18:22:17 +0100

    wip

--- packages/aflxx-nix/package.nix
+++ packages/aflxx-nix/package.nix
@@ -4,6 +4,8 @@
   aflxx,
   makeSetupHook,
   writeText,
+  withASan ? true,
+  withUBSan ? true,
 }:
 
 let
@@ -19,6 +21,8 @@ let
           aflSetupPhase() {
             export CC=${aflxx}/bin/afl-clang-lto
             export CXX=${aflxx}/bin/afl-clang-lto++
+            export AFL_USE_ASAN=${if withASan then "1" else "0"}
+            export AFL_USE_UBSAN=${if withUBSan then "1" else "0"}
           }
         ''
       );
